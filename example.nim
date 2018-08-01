0888049f4e0afd94b389d7c32cd013f10202b4b4
Tellurium52_teruruN <38907008+mzteruru52@users.noreply.github.com>
2018-08-01 21:58:47 +0900

    Update example.nim

--- example.nim
+++ example.nim
@@ -4,7 +4,6 @@
 import nmiline
 
 # = nlSize =
-
 nlSize(700,500)
 # window size (x,y :int) {defalt : (700,500)}
 
@@ -28,14 +27,15 @@ nlConfig(bgColor=(255,255,255),bgAlpha=200,textColor=(0,0,0),bdColor=(200,200,20
 # Cream  : Creamy color (bgColor=(255,245,190),textColor=(70,62,0),bdColor=(220,200,150))
 # Sky    : Color like the sky (bgColor=(180,235,255),textColor=(0,20,50),bdColor=(250,250,250))
 
+
 # = nlRange =
 nlRange(3.0,6.0,-1.0,8.0)
 # Scope of the graph (minX,maxX,minY,maxY :float)
 # If you omit it,scope is determined automatically.
 
+
 # = nlSet =
 nlSet(x=[2.0,3.0,4.0,5.0,6.0,7.0],y=[0.1,1.2,3.1,4.7,5.5,6.0],color=(255,0,0),form=Normal,point="●")
-
 # {Set datas.}
 # x and y must be same type and length.
 # color : Line's(or points') color
@@ -45,6 +45,7 @@ nlSet(x=[2.0,3.0,4.0,5.0,6.0,7.0],y=[0.1,1.2,3.1,4.7,5.5,6.0],color=(255,0,0),fo
 #         Scatter: Scatter plot
 # point : Points' shape (string)
 # name  : Data's name
+
 
 # = nlShow =
 nlShow()
